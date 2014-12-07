bbfed7fbfd9d70be3beeada7bb536d9f1d9f57ee
CarrieShi <hichunlishi@sina.cn>
2014-12-07 23:43:03 +0800

    评论插入comment表，同时转发（或处理多次转发后）的评论插入weibo表

--- weibo/Index/Lib/Action/IndexAction.class.php
+++ weibo/Index/Lib/Action/IndexAction.class.php
@@ -67,6 +67,10 @@ class IndexAction extends CommonAction {
 
 	/**
 	 * 转发微博
+	 *
+	 * isturn 保存的是原创微博的id
+	 * tid存在，则id对应的微博不是原创微博
+	 * 多重转发的信息，在页面端由js组合后再传给content
 	 */
 	public function turn() {
 		if(!$this->isPost()) {
@@ -120,44 +124,86 @@ class IndexAction extends CommonAction {
 
 	/**
 	 * 评论
+	 *
+	 * 1、评论插入comment表
+	 * 2、同时转发，评论(如果为多次转发的微博，还需处理后)插入weibo表
+	 *
+	 * $_POST['content'] 评论（或同时转发）的内容
+	 * $_POST['wid'] 被评论（或同时转发）的微博id
+	 * $_POST['uid'] 被评论（或同时转发）的用户id
+	 * $_POST['isturn'] 是否同时转发（1 ：0）
+	 *
+	 * $data['uid'] 评论（或同时转发）的用户id
+	 * $weibo['isturn'] 非空时为最早被转发的微博
 	 */
 	public function comment () {
 		if(!$this->isAjax()) {
 			halt('页面不存在！');
 		}
-		//提取评论数据
+
+		//提取异步提交的评论数据
 		$data = array(
 			'content' => $this->_post('content'),
 			'time' => time(),
-			'uid' => $this->_post('uid'),//session('uid')
+			'uid' => session('uid'),
 			'wid' => $this->_post('wid', 'intval')
 			);
+		if(M('comment')->data($data)->add()) {
+			//读取评论用户信息
+			$fields = array('username', 'face50' => 'face', 'uid');
+			$where = array('uid' => $data['uid']);
+			$user = M('userinfo')->where($where)->field($fields)->find();
+			
+			$db = M('weibo');
+			//评论数+1
+			$db->where(array('id' => $data['wid']))->setInc('comment');
+			
+			//同时转发
+			if($_POST['isturn']) {
+				$field = array('content', 'isturn');
+				$weibo = $db->field($field)->find($data['wid']);
+				//如果为多次转发的微博，还需处理 // @username : content
+				if($weibo['isturn']) {
+					$uid = $this->_post('uid', 'intval');
+					$username = M('userinfo')->where(array('uid' => $uid))->getField('username');
+				}
+				$content = $weibo['isturn'] ? $data['content'] . ' // @' . $username . ' : ' . $weibo['content'] : $data['content'];
+				
+				$cons = array(
+					'content' => $content,
+					'isturn' => $weibo['isturn'] ? $weibo['isturn'] : $data['wid'],
+					'time' => time(),
+					'uid' => $data['uid']
+					);
+				if($db->data($cons)->add()) {
+					//转发数+1
+					$db->where(array('id' => $data['wid']))->setInc('turn');
+				}
+				exit(1);
+			}
 
-		//读取评论用户信息
-		$fields = array('username', 'face50' => 'face', 'uid');
-		$where = array('uid' => $data['uid']);
-		$user = M('userinfo')->where($where)->field($fields)->find();
-		
-		//组合评论样式并以字符串返回
-		$str = '';
-		$str .= '<dl class="comment_content">';
-		$str .= '<dt><a href="' . U('/' . $data['uid']) . '">';
-		$str .= '<img src="';
-		$str .= __ROOT__;
-		if($user['face']) {
-			$str .= '/Uploads/Face/' . $user['face']; 
-		} else {
-			$str .= '/Public/Images/noface.gif';
+			//组合评论样式并以字符串返回
+			$str = '';
+			$str .= '<dl class="comment_content">';
+			$str .= '<dt><a href="' . U('/' . $data['uid']) . '">';
+			$str .= '<img src="';
+			$str .= __ROOT__;
+			if($user['face']) {
+				$str .= '/Uploads/Face/' . $user['face']; 
+			} else {
+				$str .= '/Public/Images/noface.gif';
+			}
+			$str .= '" alt="' . $user['username'] . '" width="30" height="30"/>';
+			$str .= '</a></dt><dd>';
+			$str .= '<a href="' . U('/' . $data['uid']) . '" class="comment_name">';
+			$str .= $user['username'] . '</a> : ' . replace_weibo($data['content']);
+			$str .= '&nbsp;&nbsp;( ' . time_format($data['time']) . ' )';
+			$str .= '<div class="reply">';
+			$str .= '<a href="">回复</a>';
+			$str .= '</div></dd></dl>';
+			echo $str;	
 		}
-		$str .= '" alt="' . $user['username'] . '" width="30" height="30"/>';
-		$str .= '</a></dt><dd>';
-		$str .= '<a href="' . U('/' . $data['uid']) . '" class="comment_name">';
-		$str .= $user['username'] . '</a> : ' . $data['content'];
-		$str .= '&nbsp;&nbsp;( ' . time_format($data['time']) . ' )';
-		$str .= '<div class="reply">';
-		$str .= '<a href="">回复</a>';
-		$str .= '</div></dd></dl>';
-		echo $str;
+		
 	}
 
 	/**
