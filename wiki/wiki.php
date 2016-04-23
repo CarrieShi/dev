acf257fac57372260daa41d3979718342eb70f22
CarrieShi <hichunlishi@sina.cn>
2016-04-23 16:45:05 +0800

    Update wiki.php
    添加title

--- wiki/wiki.php
+++ wiki/wiki.php
@@ -7,5 +7,6 @@ if(empty($_GET['read'])){
 
 $obj = new MdFile($_GET['read']);
 $markdown = $obj->getContent();
+$title = str_replace('.md', '', basename($_GET['read']));
 
 require 'theme/default/wiki.html';
